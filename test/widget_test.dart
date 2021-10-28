64010e4247821ca9914fede0bde469223bf3749d
hchauhan7816 <hchauhan7816@gmail.com>
2021-10-28 23:59:10 +0530

    Small todo updates

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:sadak/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const OurApp());
+    await tester.pumpWidget(OurApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
