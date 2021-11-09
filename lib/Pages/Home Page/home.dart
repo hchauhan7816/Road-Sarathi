a4d02d07161e50b525c9093c733f03f419ceb839
hchauhan7816 <hchauhan7816@gmail.com>
2021-11-09 07:10:28 +0530

    home page navigation

--- lib/Pages/Home Page/home.dart
+++ lib/Pages/Home Page/home.dart
@@ -26,6 +26,42 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   FirebaseHelper firebaseHelper = Get.find<FirebaseHelper>();
+  bool isLoading = false;
+
+  navigateUser() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    int? x;
+
+    await firebaseHelper.firebaseFirestore
+        .collection(_USERS)
+        .where(_EMAIL, isEqualTo: Constants.myEmail)
+        .get()
+        .then((value) {
+      value.docs.isNotEmpty ? x = value.docs[0].data()["status"] : x = null;
+
+      dev.log(value.docs[0].data()["status"].toString());
+    });
+
+    if (x != null) {
+      dev.log(x.toString());
+      if (x != 0) {
+        if (x == 1) {
+          Get.offAll(
+              () => GovConversationRooms(authorityEmail: _LOCALAUTHORITYMAIL));
+        } else {
+          Get.offAll(
+              () => GovConversationRooms(authorityEmail: _HIGHERAUTHORITYMAIL));
+        }
+      }
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
 
   @override
   void initState() {
@@ -34,6 +70,8 @@ class _HomePageState extends State<HomePage> {
     // dev.log("email :: $email   \n\n password :: $password");
 
     // todo - Must have status 1 or 2
+
+    navigateUser();
     // Map<String, dynamic>? x;
 
     // Get.offAll(() => GovConversationRooms(authorityEmail: _LOCALAUTHORITYMAIL));
@@ -75,126 +113,128 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width / 1.1,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              MaterialButton(
-                minWidth: double.infinity,
-                height: 150.h,
-                onPressed: () {},
-                color: Palette.darkPurple,
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(color: Palette.darkPurple),
-                  borderRadius: BorderRadius.circular(75.w),
-                ),
-                child: Text(
-                  "${firebaseHelper.auth.currentUser!.email}",
-                  style: TextStyle(
-                    fontSize: 55.sp,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width / 1.1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    MaterialButton(
+                      minWidth: double.infinity,
+                      height: 150.h,
+                      onPressed: () {},
+                      color: Palette.darkPurple,
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: Palette.darkPurple),
+                        borderRadius: BorderRadius.circular(75.w),
+                      ),
+                      child: Text(
+                        "${firebaseHelper.auth.currentUser!.email}",
+                        style: TextStyle(
+                          fontSize: 55.sp,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100.h,
+                    ),
+                    MaterialButton(
+                      minWidth: double.infinity,
+                      height: 150.h,
+                      onPressed: () {
+                        firebaseHelper.signout();
+                      },
+                      color: Palette.darkPurple,
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: Palette.darkPurple),
+                        borderRadius: BorderRadius.circular(75.w),
+                      ),
+                      child: Text(
+                        "Logout",
+                        style: TextStyle(
+                          fontSize: 55.sp,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100.h,
+                    ),
+                    MaterialButton(
+                      minWidth: double.infinity,
+                      height: 150.h,
+                      onPressed: () {
+                        Get.to(UserConversationRooms());
+                      },
+                      color: Palette.darkPurple,
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: Palette.darkPurple),
+                        borderRadius: BorderRadius.circular(75.w),
+                      ),
+                      child: Text(
+                        "Local Authority",
+                        style: TextStyle(
+                          fontSize: 55.sp,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100.h,
+                    ),
+                    MaterialButton(
+                      minWidth: double.infinity,
+                      height: 150.h,
+                      onPressed: () {
+                        Get.to(UserHigherConversationRooms());
+                      },
+                      color: Palette.darkPurple,
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: Palette.darkPurple),
+                        borderRadius: BorderRadius.circular(75.w),
+                      ),
+                      child: Text(
+                        "Higher Authority",
+                        style: TextStyle(
+                          fontSize: 55.sp,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100.h,
+                    ),
+                    MaterialButton(
+                      minWidth: double.infinity,
+                      height: 150.h,
+                      onPressed: () {
+                        Get.to(ComplaintPage());
+                      },
+                      color: Palette.darkPurple,
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: Palette.darkPurple),
+                        borderRadius: BorderRadius.circular(75.w),
+                      ),
+                      child: Text(
+                        "Create New Complaints",
+                        style: TextStyle(
+                          fontSize: 55.sp,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(
-                height: 100.h,
-              ),
-              MaterialButton(
-                minWidth: double.infinity,
-                height: 150.h,
-                onPressed: () {
-                  firebaseHelper.signout();
-                },
-                color: Palette.darkPurple,
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(color: Palette.darkPurple),
-                  borderRadius: BorderRadius.circular(75.w),
-                ),
-                child: Text(
-                  "Logout",
-                  style: TextStyle(
-                    fontSize: 55.sp,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 100.h,
-              ),
-              MaterialButton(
-                minWidth: double.infinity,
-                height: 150.h,
-                onPressed: () {
-                  Get.to(UserConversationRooms());
-                },
-                color: Palette.darkPurple,
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(color: Palette.darkPurple),
-                  borderRadius: BorderRadius.circular(75.w),
-                ),
-                child: Text(
-                  "Local Authority",
-                  style: TextStyle(
-                    fontSize: 55.sp,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 100.h,
-              ),
-              MaterialButton(
-                minWidth: double.infinity,
-                height: 150.h,
-                onPressed: () {
-                  Get.to(UserHigherConversationRooms());
-                },
-                color: Palette.darkPurple,
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(color: Palette.darkPurple),
-                  borderRadius: BorderRadius.circular(75.w),
-                ),
-                child: Text(
-                  "Higher Authority",
-                  style: TextStyle(
-                    fontSize: 55.sp,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 100.h,
-              ),
-              MaterialButton(
-                minWidth: double.infinity,
-                height: 150.h,
-                onPressed: () {
-                  Get.to(ComplaintPage());
-                },
-                color: Palette.darkPurple,
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(color: Palette.darkPurple),
-                  borderRadius: BorderRadius.circular(75.w),
-                ),
-                child: Text(
-                  "Create New Complaints",
-                  style: TextStyle(
-                    fontSize: 55.sp,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
--- lib/app.dart
+++ lib/app.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sadak/Config/constants.dart';
 import 'package:sadak/Pages/Home%20Page/home.dart';
 import 'package:sadak/Pages/Login%20Signup%20Page/login_gov.dart';
 import 'package:sadak/Pages/On%20Boarding/on_boarding.dart';
 import 'package:get/get.dart';
-
 import 'Config/themes.dart';
 import 'Pages/Chat Screen/chat_screen.dart';
 import 'Pages/Conversation Rooms/conversation_rooms.dart';
 import 'Pages/Login Signup Page/login.dart';
 import 'Pages/Signup Page/signup.dart';
 import 'Services/Controllers/auth_controller.dart';
+import 'dart:developer' as dev;
 
-class OurApp extends StatelessWidget {
+class OurApp extends StatefulWidget {
   OurApp({Key? key}) : super(key: key);
 
+  static const String _USERS = "users";
+  static const String _EMAIL = "email";
+
+  @override
+  State<OurApp> createState() => _OurAppState();
+}
+
+class _OurAppState extends State<OurApp> {
   FirebaseHelper firebaseHelper = Get.find<FirebaseHelper>();
+  bool isGovern = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Constants.myEmail = firebaseHelper.auth.currentUser!.email!;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +49,16 @@ class OurApp extends StatelessWidget {
         //         authorityEmail: "localauthority@gmail.com") //HomePage()
         //     : const OnBoarding(),
 
+        // home: firebaseHelper.auth.currentUser != null
+        //     ? (isGovernment()
+        //         ? GovConversationRooms(
+        //             authorityEmail: "localauthority@gmail.com")
+        //         : HomePage())
+        //     : OnBoarding(),
+
         // Todo update the path to home page back
-        home: OnBoarding(),
+        // home: OnBoarding(),
+        home: HomePage(),
 
         // getPages: [
         //   GetPage(name: '/on_boarding', page: () => const OnBoarding()),
@@ -46,4 +71,34 @@ class OurApp extends StatelessWidget {
       ),
     );
   }
+
+  // bool isGovernment() {
+  //   int? x;
+  //   firebaseHelper.firebaseFirestore
+  //       .collection(OurApp._USERS)
+  //       .where(OurApp._EMAIL, isEqualTo: Constants.myEmail)
+  //       .get()
+  //       .then((value) {
+  //     value.docs.isNotEmpty ? x = value.docs[0].data()["status"] : x = null;
+  //   });
+
+  //   //   dev.log(value.docs[0].data()["status"].toString());
+  //   // });
+
+  //   dev.log("Here");
+  //   dev.log(x.toString());
+
+  //   if (x != null) {
+  //     dev.log(x.toString());
+  //     if (x != 0) {
+  //       return true;
+  //       // Get.offAll(
+  //       // () => GovConversationRooms(authorityEmail: _HIGHERAUTHORITYMAIL));
+
+  //     }
+  //   }
+
+  //   dev.log("Here");
+  //   return false;
+  // }
 }
